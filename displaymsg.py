7489c81e195231db58e697d918e5205ddfb472e0
sersangn <sersang01@gmail.com>
2024-08-15 20:12:01 +0800

    more update

--- displaymsg.py
+++ displaymsg.py
@@ -1,14 +1,21 @@
 import platform
 import os
 import ctypes
+import subprocess
 
 def display_message():
     os_type = platform.system()
     if os_type == "Windows":
         ctypes.windll.user32.MessageBoxW(0, "You've been infected by HydraInfect!", "Malware Alert", 1)
     elif os_type == "Darwin":  # macOS
-        os.system('osascript -e \'display dialog "You\'ve been infected by HydraInfect!"\'')
+        try:
+            subprocess.run(['osascript', '-e', 'display dialog "You\'ve been infected by HydraInfect!"'])
+        except Exception as e:
+            print(f"Failed to display message on macOS: {e}")
     elif os_type == "Linux":
-        os.system('zenity --info --text="You\'ve been infected by HydraInfect!"')
+        try:
+            subprocess.run(['zenity', '--info', '--text=You\'ve been infected by HydraInfect!'])
+        except Exception as e:
+            print(f"Failed to display message on Linux: {e}")
     else:
         print("You've been infected by HydraInfect!")  # Fallback for other OSes
